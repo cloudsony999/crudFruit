9f7579740673284ad9dd5a11feedbcb75f19ea61
cloudsony999 <cloudsony999@gmail.com>
2023-03-29 17:03:39 +0530

    commit

---
+++ src/app/fruits/fruits.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FruitsRoutingModule } from './fruits-routing.module';
+import { HomeComponent } from './home/home.component';
+import { CreateComponent } from './create/create.component';
+import { FormsModule } from '@angular/forms';
+
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    CreateComponent
+  ],
+  imports: [
+    CommonModule,
+    FruitsRoutingModule,
+    FormsModule
+  ]
+})
+export class FruitsModule { }
